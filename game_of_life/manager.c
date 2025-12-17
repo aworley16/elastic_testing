26c5d36c600f63f839096fc3a72075d94db3d9f5
aworley16 <apworley@gmail.com>
2025-12-17 10:46:58 -0600

    test to see if merge merges world or just parent and child comm

--- game_of_life/manager.c
+++ game_of_life/manager.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
    MPI_Comm universe;           /* intracommunicator containing MPI_COMM_WORLD and the spawned processes */	
  
    if(original_rank == 0){
-	   MPI_Comm_spawn("worker.exe", MPI_ARGV_NULL, 2, 
+	   MPI_Comm_spawn("worker.exe", MPI_ARGV_NULL, 1, 
                  MPI_INFO_NULL, 0, MPI_COMM_SELF, &everyone, 
                  MPI_ERRCODES_IGNORE);
 				  
@@ -28,17 +28,17 @@ int main(int argc, char *argv[])
 		//int message2 = 44;
 		//MPI_Send(&message, 1, MPI_INT, 0, 0, everyone);
 		//MPI_Send(&message2, 1, MPI_INT, 1, 1, everyone);
-		
+   
 		
 		MPI_Intercomm_merge(everyone, 1, &universe);
 		
 		//see size and rank of universe;
-		//int universe_size = -1;
-		//int universe_rank = -1;
-		//MPI_Comm_rank(universe, &universe_rank);
-		//MPI_Comm_size(universe, &universe_size);
-		//printf("ORIGINAL_RANK 0, universe %d of %d \n", universe_rank, universe_size);
-   }		
+		int universe_size = -1;
+		int universe_rank = -1;
+		MPI_Comm_rank(universe, &universe_rank);
+		MPI_Comm_size(universe, &universe_size);
+		printf("ORIGINAL_RANK 0, universe %d of %d \n", universe_rank, universe_size);
+   }	
  
    MPI_Finalize(); 
    return 0; 
