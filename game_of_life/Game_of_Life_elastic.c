0233f182a0ec826eb4644307a457cde5fae6be87
aworley16 <apworley@gmail.com>
2026-01-05 14:11:09 -0600

    removed phase break barrier

--- game_of_life/Game_of_Life_elastic.c
+++ game_of_life/Game_of_Life_elastic.c
@@ -282,7 +282,7 @@ int main(int argc, char *argv[])
 	
 	//Have root print the results
 		if(global_rank == 0 ){
-		printf("Exeution time: %f\n", time);
+		printf("Exeution time: %f\n", endtime);
     } 
     MPI_Finalize();
 
