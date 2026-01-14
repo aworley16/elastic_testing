f14b725959e43b7bd5fd403e0767de598976a816
aworley16 <apworley@gmail.com>
2026-01-14 10:58:07 -0600

    changed EOL

--- game_of_life/run.sh
+++ game_of_life/run.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 #SBATCH --account=phd-apworley42
-#SBATCH --cpus-per-task=16
+#SBATCH --cpus-per-task=32
 
 module load gnu9
 module load mpich
 
-mpirun -n 1 ./gol.exe
-mpirun -n 2 ./gol.exe
-mpirun -n 3 ./gol.exe
-mpirun -n 4 ./gol.exe
-mpirun -n 5 ./gol.exe
-mpirun -n 6 ./gol.exe
-mpirun -n 7 ./gol.exe
-mpirun -n 8 ./gol.exe
-mpirun -n 16 ./gol.exe
+mpirun -n 1 ./gol.exe > out1.csv
+mpirun -n 2 ./gol.exe > out2.csv
+mpirun -n 4 ./gol.exe > out4.csv
+mpirun -n 8 ./gol.exe > out8.csv
+mpirun -n 16 ./gol.exe > out16.csv
+mpirun -n 32 ./gol.exe > out32.csv
