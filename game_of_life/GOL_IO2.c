953ab015d2568afbaee5b0e571ebcf46a098a00f
aworley16 <apworley@gmail.com>
2026-02-13 10:15:07 -0600

    debug stall

--- game_of_life/GOL_IO2.c
+++ game_of_life/GOL_IO2.c
@@ -107,7 +107,7 @@ double setup_comms(int N, int phase, int phase_size, MPI_Comm* universe, int* co
 	//if exactly enough processes exist for phase, just use current universe. 
 	if(uni_size == phase_size){
 		printf("%d SKIP \n", uni_rank);
-		if(local != NULL){*change = 0; *color=1}else{*change=1;} 
+		if(local != NULL){*change = 0; *color=1;}else{*change=1;} 
 		return 0;
 	}
 	// if too many processes exist, split and send kill flag (color == 0) to unneeded processes. 
